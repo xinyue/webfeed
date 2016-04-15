86e2a56508633ba7880f04629d0a0b2e290b8c27
mtrencseni <mtrencseni@gmail.com>
2016-04-15 12:49:12 +0000

    Add torrentproxy and imdb to piratebay webfeeder.

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from google.appengine.api import urlfetch
 class Link(ndb.Model):
     url = ndb.StringProperty(indexed=True)
 
-def get_urls(from_url, to_prefix):
-    html = lxml.html.fromstring(urlfetch.fetch(from_url, validate_certificate=False).content)
-    logging.debug('Downloaded %s' % from_url)
-    html.make_links_absolute(from_url)
+def get_urls(html, to_prefix):
     urls = html.xpath('//a/@href')
     matching_urls = []
     for url in urls:
@@ -21,9 +18,13 @@ def get_urls(from_url, to_prefix):
             matching_urls.append(str(url))
     return matching_urls
 
-def get_page_title(url):
+def get_html(url):
     html = lxml.html.fromstring(urlfetch.fetch(url, validate_certificate=False).content)
     logging.debug('Downloaded %s' % url)
+    html.make_links_absolute(url)
+    return html
+
+def get_title(html):
     page_title = html.xpath("//title")[0].text.encode('utf-8')
     logging.debug('Title: %s' % page_title)
     return page_title
@@ -35,14 +36,25 @@ def get_hn_post(post_id):
     url = 'https://hacker-news.firebaseio.com/v0/item/%s.json' % post_id
     return get_json(url)
 
-def main(from_url, to_prefix, from_email, to_email, subject_prefix):
+def main(from_url, to_prefix, replace_with, attach_url_prefix, from_email, to_email, subject_prefix):
     logging.getLogger().setLevel(logging.DEBUG)
-    urls = get_urls(from_url, to_prefix)
+    urls = get_urls(get_html(from_url), to_prefix)
     for url in urls:
         if len(Link.query(Link.url == url).fetch(1)) == 0:
-            title = get_page_title(url)
+            html = get_html(url)
+            title = get_title(html)
             subject = "[%s] %s" % (subject_prefix, title)
-            body = url
+            attach_url = None
+            if attach_url_prefix is not None:
+                attach_urls = get_urls(html, attach_url_prefix)
+                if (len(attach_urls) > 0):
+                    attach_url = attach_urls[0]
+            if replace_with is not None:
+                body = url.replace(to_prefix, replace_with)
+            else:
+                body = url
+            if attach_url is not None:
+                body += "\n\n%s" % attach_url
             mail.send_mail(from_email, to_email, subject, body)
             Link(url=url).put()
 
@@ -50,6 +62,8 @@ class Bluesnews(webapp2.RequestHandler):
     def get(self):
         main(from_url='http://bluesnews.com',
              to_prefix='https://www.youtube.com',
+             replace_with=None,
+             attach_url_prefix=None,
              from_email='Bluesnews Youtube Watcher <mtrencseni@gmail.com>',
              to_email='Marton Trencseni <mtrencseni@gmail.com>',
              subject_prefix='bluesnews')
@@ -58,6 +72,8 @@ class Piratebay(webapp2.RequestHandler):
     def get(self):
         main(from_url='https://thepiratebay.se/top/207',
              to_prefix='https://thepiratebay.se/torrent',
+             replace_with='https://pirateproxy.tv/torrent',
+             attach_url_prefix='http://www.imdb.com',
              from_email='PirateBay Watcher <mtrencseni@gmail.com>',
              to_email='Marton Trencseni <mtrencseni@gmail.com>',
              subject_prefix='piratebay')
