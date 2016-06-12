d456422c5a5c6d01011a0f17948fe63e13635d2b
mtrencseni <mtrencseni@gmail.com>
2016-06-12 12:21:05 +0000

    Added Evan Miller, Simply Statistics and Backreaction.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import traceback
 import lxml.html
 import webapp2
 import logging
@@ -9,7 +10,31 @@ from google.appengine.api import urlfetch
 class Link(ndb.Model):
     url = ndb.StringProperty(indexed=True)
 
-def get_urls(html, to_prefix, xpath):
+pirate_bay_proxies = [
+    'https://thepiratebay.se',
+    'https://pirateproxy.tv',
+    'http://tpb.portalimg.com/?load=',
+    'https://thepiratebay.run/?load=',
+    'https://pbp.wtf',
+    'https://bayproxy.link',
+    'https://tpb.press'
+    ]
+pi = 0
+
+def url_fetch(url):
+    # huge hack
+    global pi
+    for i in xrange(len(pirate_bay_proxies)):
+        try:
+            url = url.replace('https://thepiratebay.se', pirate_bay_proxies[pi])
+            return urlfetch.fetch(url, validate_certificate=False).content
+        except Exception, e:
+            logging.debug('url_fetch exception: %s' % e)
+            # try another proxy
+            pi = (pi + 1) % len(pirate_bay_proxies)
+    raise Exception
+
+def get_urls(html, to_prefix, xpath='//a/@href'):
     urls = html.xpath(xpath)
     matching_urls = []
     for url in urls:
@@ -19,7 +44,7 @@ def get_urls(html, to_prefix, xpath):
     return matching_urls
 
 def get_html(url):
-    html = lxml.html.fromstring(urlfetch.fetch(url, validate_certificate=False).content)
+    html = lxml.html.fromstring(url_fetch(url))
     logging.debug('Downloaded %s' % url)
     html.make_links_absolute(url)
     return html
@@ -30,7 +55,7 @@ def get_title(html):
     return page_title
 
 def get_json(url):
-    return json.loads(urlfetch.fetch(url, validate_certificate=False).content)
+    return json.loads(url_fetch(url))
 
 def get_hn_post(post_id):
     url = 'https://hacker-news.firebaseio.com/v0/item/%s.json' % post_id
@@ -40,23 +65,63 @@ def main(from_url, xpath, to_prefix, replace_with, attach_url_prefix, from_email
     logging.getLogger().setLevel(logging.DEBUG)
     urls = get_urls(get_html(from_url), to_prefix, xpath)
     for url in urls:
-        if len(Link.query(Link.url == url).fetch(1)) == 0:
-            html = get_html(url)
-            title = get_title(html)
-            subject = "[%s] %s" % (subject_prefix, title)
-            attach_url = None
-            if attach_url_prefix is not None:
-                attach_urls = get_urls(html, attach_url_prefix)
-                if (len(attach_urls) > 0):
-                    attach_url = attach_urls[0]
-            if replace_with is not None:
-                body = url.replace(to_prefix, replace_with)
-            else:
-                body = url
-            if attach_url is not None:
-                body += "\n\n%s" % attach_url
-            mail.send_mail(from_email, to_email, subject, body)
-            Link(url=url).put()
+        try:
+            if len(Link.query(Link.url == url).fetch(1)) == 0:
+                html = get_html(url)
+                title = get_title(html)
+                subject = "[%s] %s" % (subject_prefix, title)
+                attach_url = None
+                if attach_url_prefix is not None:
+                    attach_urls = get_urls(html, attach_url_prefix)
+                    if (len(attach_urls) > 0):
+                        attach_url = attach_urls[0]
+                if replace_with is not None:
+                    if replace_with == '__PIRATEBAY__':
+                        body = url.replace(to_prefix, pirate_bay_proxies[pi] + '/torrent')
+                    else:
+                        body = url.replace(to_prefix, replace_with)
+                else:
+                    body = url
+                if attach_url is not None:
+                    body += "\n\n%s" % attach_url
+                mail.send_mail(from_email, to_email, subject, body)
+                Link(url=url).put()
+        except Exception, err:
+            logging.debug('Exception while fetching %s' % url)
+            logging.debug(traceback.format_exc())
+
+class Backreaction(webapp2.RequestHandler):
+    def get(self):
+        main(from_url='https://backreaction.blogspot.com',
+             xpath='//h3/a/@href',
+             to_prefix='https://backreaction.blogspot.com/',
+             replace_with=None,
+             attach_url_prefix=None,
+             from_email='Webfeed <mtrencseni@gmail.com>',
+             to_email='Marton Trencseni <mtrencseni@gmail.com>',
+             subject_prefix='backreaction')
+
+class Simplystatistics(webapp2.RequestHandler):
+    def get(self):
+        main(from_url='http://simplystatistics.org/',
+             xpath='//h1/a/@href',
+             to_prefix='http://simplystatistics.org/',
+             replace_with=None,
+             attach_url_prefix=None,
+             from_email='Webfeed <mtrencseni@gmail.com>',
+             to_email='Marton Trencseni <mtrencseni@gmail.com>',
+             subject_prefix='simplystatistics')
+
+class Evanmiller(webapp2.RequestHandler):
+    def get(self):
+        main(from_url='http://www.evanmiller.org/',
+             xpath='//a/@href',
+             to_prefix='http://www.evanmiller.org/',
+             replace_with=None,
+             attach_url_prefix=None,
+             from_email='Webfeed <mtrencseni@gmail.com>',
+             to_email='Marton Trencseni <mtrencseni@gmail.com>',
+             subject_prefix='evanmiller')
 
 class Bluesnews(webapp2.RequestHandler):
     def get(self):
@@ -65,20 +130,31 @@ class Bluesnews(webapp2.RequestHandler):
              to_prefix='https://www.youtube.com',
              replace_with=None,
              attach_url_prefix=None,
-             from_email='Bluesnews Youtube Watcher <mtrencseni@gmail.com>',
+             from_email='Webfeed <mtrencseni@gmail.com>',
              to_email='Marton Trencseni <mtrencseni@gmail.com>',
              subject_prefix='bluesnews')
 
-class Piratebay(webapp2.RequestHandler):
+class PiratebayMovies(webapp2.RequestHandler):
     def get(self):
         main(from_url='https://thepiratebay.se/top/207',
              xpath='//a/@href',
              to_prefix='https://thepiratebay.se/torrent',
-             replace_with='https://pirateproxy.tv/torrent',
+             replace_with='__PIRATEBAY__',
              attach_url_prefix='http://www.imdb.com',
-             from_email='PirateBay Watcher <mtrencseni@gmail.com>',
+             from_email='Webfeed <mtrencseni@gmail.com>',
              to_email='Marton Trencseni <mtrencseni@gmail.com>',
-             subject_prefix='piratebay')
+             subject_prefix='piratebay movies')
+
+class PiratebayGames(webapp2.RequestHandler):
+    def get(self):
+        main(from_url='https://thepiratebay.se/top/401',
+             xpath='//a/@href',
+             to_prefix='https://thepiratebay.se/torrent',
+             replace_with='__PIRATEBAY__',
+             attach_url_prefix=None,
+             from_email='Webfeed <mtrencseni@gmail.com>',
+             to_email='Marton Trencseni <mtrencseni@gmail.com>',
+             subject_prefix='piratebay games')
 
 class Redlettermedia(webapp2.RequestHandler):
     def get(self):
@@ -87,7 +163,7 @@ class Redlettermedia(webapp2.RequestHandler):
              to_prefix='https://www.youtube.com/embed/',
              replace_with='https://www.youtube.com/watch?v=',
              attach_url_prefix=None,
-             from_email='RedLetterMedia Watcher <mtrencseni@gmail.com>',
+             from_email='Webfeed <mtrencseni@gmail.com>',
              to_email='Marton Trencseni <mtrencseni@gmail.com>',
              subject_prefix='redlettermedia')
 
@@ -106,7 +182,7 @@ class Hackernews(webapp2.RequestHandler):
                     hn_url = 'https://news.ycombinator.com/item?id=%s' % post['id']
                     subject = "[hackernews] %s" % post['title']
                     body = '%s\n\n%s' % (hn_url, post['url'])
-                    from_email = 'Hacker News Watcher <mtrencseni@gmail.com>'
+                    from_email='Webfeed <mtrencseni@gmail.com>',
                     to_email = 'Marton Trencseni <mtrencseni@gmail.com>'
                     mail.send_mail(from_email, to_email, subject, body)
                     Link(url=post['url']).put()
@@ -117,8 +193,12 @@ class Hackernews(webapp2.RequestHandler):
                 self.response.write('- does not qualify\n')
 
 app = webapp2.WSGIApplication([
+    ('/backreaction', Backreaction),
+    ('/simplystatistics', Simplystatistics),
+    ('/evanmiller', Evanmiller),
     ('/bluesnews', Bluesnews),
-    ('/piratebay', Piratebay),
+    ('/piratebay_movies', PiratebayMovies),
+    ('/piratebay_games', PiratebayGames),
     ('/redlettermedia', Redlettermedia),
     ('/hackernews', Hackernews),
 ], debug=True)
